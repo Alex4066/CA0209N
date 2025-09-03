9a6cb5403214129e701fc9b9283b02bee0557f57
unknown <lab1056@etecjk.local>
2025-09-02 22:22:00 -0300

    testando

--- 0209N/Program.cs
+++ 0209N/Program.cs
@@ -1,15 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace _0209N
 {
-    internal class Program
+    internal class Pessoa
     {
-        static void Main(string[] args)
-        {
-        }
-    }
+        public String CPF;
+        public String Nome;
+        public int Idade;
+        public string Endereço;
+        public string Email;
+
+        public void Cadastrar()
+        public void Consultar()
 }
+}
+    {
+    internal class Funcionario : Pessoa
+    public int Registro;
+    public double Salario;
+    public string Funcao;
+    public string Admissao;
+    public double ValeRefeicao;
+    public double ValeTransporte;
+    public double ValeAlimentacao;
+    public string AssistenciaMedica;
+    
+}
+
+
+        
